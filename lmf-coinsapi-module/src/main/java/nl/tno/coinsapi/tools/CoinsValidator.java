d33cca58a27657cf985dcf4cadeafd7368c46d6a
johankunnen <J.Kunnen@tno.nl>
2014-08-25 16:08:11 +0200

    Add validation

---
+++ lmf-coinsapi-module/src/main/java/nl/tno/coinsapi/tools/CoinsValidator.java
@@ -0,0 +1,144 @@
+package nl.tno.coinsapi.tools;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Vector;
+
+import nl.tno.coinsapi.services.CoinsApiService;
+import nl.tno.coinsapi.services.ICoinsApiService.ValidationAspect;
+
+import org.apache.marmotta.platform.core.exception.MarmottaException;
+import org.apache.marmotta.platform.sparql.api.sparql.SparqlService;
+import org.openrdf.model.Value;
+import org.openrdf.query.QueryLanguage;
+
+/**
+ * Coins Validator Object
+ */
+public abstract class CoinsValidator {
+
+	protected String mContext = null;
+	protected SparqlService mSparqlService = null;
+	protected List<String> mErrors = new Vector<String>();
+	
+	/**
+	 * @return true if OK
+	 */
+	public abstract boolean validate();
+	
+	/**
+	 * Set the context
+	 * @param pContext
+	 */
+	public void setContext(String pContext) {
+		mContext = pContext; 
+	}
+
+	/**
+	 * @param pSparqlService
+	 */
+	public void setSparqlService(SparqlService pSparqlService) {
+		mSparqlService = pSparqlService;
+	}
+
+	/**
+     * @return a list of errors (only if validate returns false)
+	 */
+	public List<String> getValidationErrors() {
+		return mErrors;
+	}
+
+	/**
+	 * Validator validating all aspects
+	 */
+	public static class CoinsAllValidator extends CoinsValidator {
+
+		private List<CoinsValidator> mValidators = new Vector<CoinsValidator>();
+		
+		/**
+		 * Constructor
+		 */
+		public CoinsAllValidator() {
+			for (ValidationAspect aspect : ValidationAspect.values()) {
+				switch (aspect) {
+				case ALL:
+					break;
+				case PHYSICALPARENT:
+					mValidators.add(new CoinsPhysicalParentValidator());
+					break;
+				}
+			}
+		}
+		
+		@Override
+		public boolean validate() {
+			mErrors.clear();
+			for (CoinsValidator validator : mValidators) {
+				if (!validator.validate()) {
+					for (String error : validator.getValidationErrors()) {
+						mErrors.add(error);
+					}
+				}
+			}
+			return (mErrors.size() == 0);
+		}
+
+		@Override
+		public void setContext(String pContext) {
+			for (CoinsValidator validator : mValidators) {
+				validator.setContext(pContext);
+			}
+		}
+
+		@Override
+		public void setSparqlService(SparqlService pSparqlService) {
+			for (CoinsValidator validator : mValidators) {
+				validator.setSparqlService(pSparqlService);
+			}
+		}
+
+		@Override
+		public List<String> getValidationErrors() {
+			return mErrors;
+		}
+	}
+
+	/**
+	 * Physical Parent validator
+	 */
+	public static class CoinsPhysicalParentValidator extends CoinsValidator {
+		
+		@Override
+		public boolean validate() {
+			boolean isOk = true;
+			mErrors.clear();
+			Set<String> children = new HashSet<String>(); 
+			StringBuilder query = new StringBuilder();
+			query.append("PREFIX ");
+			query.append(CoinsApiService.PREFIX_CBIM);
+			query.append("\n\nSELECT ?child ?parent WHERE {\n\tGRAPH <");
+			query.append(mContext);
+			query.append("> {\n\t\t?child cbim:physicalParent ?parent }}");
+			List<Map<String, Value>> result = null;
+			try {
+				result = mSparqlService.query(QueryLanguage.SPARQL, query.toString());
+			} catch (MarmottaException e) {
+				e.printStackTrace();
+				mErrors.add("MarmottaException");
+				return false;
+			}
+			for (Map<String, Value> item : result) {
+				String child = item.get("child").stringValue();
+				if (!children.add(child)) {
+					mErrors.add("Object <" + child + "> has multiple parents.");
+					isOk = false;
+				}
+			}
+			return isOk;
+		}
+		
+	}
+	
+}
---
+++ lmf-coinsapi-module/src/test/java/nl/tno/coinsapi/webservices/TestCoinsValidator.java
@@ -0,0 +1,153 @@
+package nl.tno.coinsapi.webservices;
+
+import static com.jayway.restassured.RestAssured.given;
+
+import java.util.List;
+
+import junit.framework.Assert;
+
+import org.apache.marmotta.platform.core.test.base.JettyMarmotta;
+import org.apache.marmotta.platform.core.webservices.io.ExportWebService;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.response.ResponseBody;
+
+/**
+ * Test of coins validator
+ */
+public class TestCoinsValidator {
+
+	private static final int OK = 200;
+
+	private static JettyMarmotta marmotta;
+
+	private static final String MODEL_URI = "http://www.coinsweb.nl/testcase/zitbank.owl";
+
+	private static String context;
+
+	private static String creatorId;
+	
+	/**
+	 * Setting up Marmotta test service and RestAssured
+	 */
+	@BeforeClass
+	public static void setUp() {
+		marmotta = new JettyMarmotta("/marmotta", CoinsApiWebService.class,
+				ExportWebService.class);
+
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = marmotta.getPort();
+		RestAssured.basePath = marmotta.getContext();
+		context = RestAssured.baseURI + ":" + RestAssured.port
+				+ RestAssured.basePath;
+		
+		// POST PersonOrOrganisation (creator/modifier)
+		ResponseBody body = given()
+				.header("Content-Type", CoinsApiWebService.MIME_TYPE)
+				.queryParam("context", context)
+				.queryParam("modelURI", MODEL_URI)
+				.queryParam("name", "Pietje Puk")
+				.expect()
+				.statusCode(OK)
+				.when()
+				.post(CoinsApiWebService.PATH
+						+ CoinsApiWebService.PATH_PERSON_OR_ORGANISATION)
+				.body();
+		creatorId = body.asString();
+	}
+
+	/**
+	 * Tear down
+	 */
+	@AfterClass
+	public static void tearDown() {
+		marmotta.shutdown();
+	}
+
+	/**
+	 * Duplicate parents
+	 */
+	@Test
+	public void testDuplicateParents() {
+		String zitBankId = addPhysicalObject("Zitbank", "B1", 0);
+		String zitSysteemId = addPhysicalObject("Zitsysteem", "B1.1", 1);
+		String fundatieSysteemId = addPhysicalObject("Fundatiesysteem", "B1.2",
+				1);
+		given().header("Content-Type", CoinsApiWebService.MIME_TYPE)
+				.queryParam("context", context)
+				.queryParam("child", zitSysteemId)
+				.queryParam("parent", zitBankId)
+				.queryParam("modifier", creatorId)
+				.expect()
+				.statusCode(OK)
+				.when()
+				.post(CoinsApiWebService.PATH
+						+ CoinsApiWebService.PATH_LINK_PHYSICAL_PARENT).body();
+		given().header("Content-Type", CoinsApiWebService.MIME_TYPE)
+				.queryParam("context", context)
+				.queryParam("child", fundatieSysteemId)
+				.queryParam("parent", zitBankId)
+				.queryParam("modifier", creatorId)
+				.expect()
+				.statusCode(OK)
+				.when()
+				.post(CoinsApiWebService.PATH
+						+ CoinsApiWebService.PATH_LINK_PHYSICAL_PARENT).body();		
+		ResponseBody body = given().header("Content-Type", CoinsApiWebService.MIME_TYPE)
+				.queryParam("context", context)
+				.queryParam("aspect", "physicalParent")
+				.expect()
+				.statusCode(OK)
+				.when()
+				.get(CoinsApiWebService.PATH
+						+ CoinsApiWebService.PATH_VALIDATE).body();
+		@SuppressWarnings("unchecked")
+		List<String> result = body.as(List.class);
+		Assert.assertEquals(1, result.size());
+		Assert.assertEquals("OK", result.get(0));
+		// Now add a duplicate...
+		body = given().header("Content-Type", CoinsApiWebService.MIME_TYPE)
+				.queryParam("context", context)
+				.queryParam("object", fundatieSysteemId)
+				.queryParam("name", "cbim:physicalParent")
+				.queryParam("value", zitSysteemId)
+				.expect()
+				.statusCode(OK)
+				.when()
+				.post(CoinsApiWebService.PATH
+						+ CoinsApiWebService.PATH_ADD_ATTRIBUTE_RESOURCE).body();
+		body = given().header("Content-Type", CoinsApiWebService.MIME_TYPE)
+				.queryParam("context", context)
+				.queryParam("aspect", "physicalParent")
+				.expect()
+				.statusCode(OK)
+				.when()
+				.get(CoinsApiWebService.PATH
+						+ CoinsApiWebService.PATH_VALIDATE).body();
+		@SuppressWarnings("unchecked")
+		List<String> result2 = body.as(List.class);
+		Assert.assertEquals(1, result2.size());
+		Assert.assertTrue(result2.get(0).contains("multiple parents"));
+	}
+	
+	private String addPhysicalObject(String name, String userId, int layerIndex) {
+		ResponseBody body = given()
+				.header("Content-Type", CoinsApiWebService.MIME_TYPE)
+				.queryParam("context", context)
+				.queryParam("modelURI", MODEL_URI)
+				.queryParam("name", name)
+				.queryParam("layerIndex", layerIndex)
+				.queryParam("creator", creatorId)
+				.queryParam("userID", userId)
+				.expect()
+				.statusCode(OK)
+				.when()
+				.post(CoinsApiWebService.PATH
+						+ CoinsApiWebService.PATH_PHYSICAL_OBJECT).body();
+		return body.asString();
+	}
+	
+}
